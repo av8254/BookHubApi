1af133c36157dca81769769e7a29db21665a29dd
Alen Vidmar <alenvidmar3@gmail.com>
2023-01-08 14:28:17 +0100

    image link fix

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
@@ -21,7 +22,7 @@ class BookFactory extends Factory
             'tags' => $this->faker->word,
             'description' => $this->faker->paragraph(5),
             'author' => fake()->name(),
-            'image' => asset('storage/images/' . $this->faker->randomElement(['book1.png','book2.png','book3.png']))
+            'image' => 'https://trolebus.si/storage/images/' . $this->faker->randomElement(['book1.png', 'book2.png', 'book3.png'])
         ];
     }
 }
